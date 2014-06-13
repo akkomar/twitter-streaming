cdf212d212099b1b9b29e863aea1812c54b87a07
akkomar <ak.komar@gmail.com>
2014-06-13 20:49:48 +0200

    Implemented printing 10 most popular hashtags in last 60s (counting over 60s window moving every 1s)

--- twitter-streaming-spark/src/main/scala/TwitterSparkStreaming.scala
+++ twitter-streaming-spark/src/main/scala/TwitterSparkStreaming.scala
@@ -1,12 +1,38 @@
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.receiver.Receiver
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter.TwitterUtils
+import twitter4j.auth.OAuthAuthorization
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object TwitterSparkStreaming {
   def main(args: Array[String]) {
-    val ssc = new StreamingContext("local", "TwitterFeed", Seconds(1))
-//    val tweets = ssc.stream
+
+    val ssc = new StreamingContext("local[3]", "TwitterFeed", Seconds(1))
+    ssc.checkpoint("checkpoint/")
+
+    val tweets = TwitterUtils.createStream(ssc, Some(new OAuthAuthorization(Util.config)))
+    //    val count = tweets.count()
+    //    count.print()
+
+    val statuses = tweets.map(_.getText)
+    val words = statuses.flatMap(_.split("[\\s\\,]"))
+    val hashtags = words.filter(_.startsWith("#"))
+    //    hashtags.print()
+    val counts = hashtags.map((_, 1)).reduceByKeyAndWindow(_ + _, _ - _, Seconds(60), Seconds(1))
+    val sortedCounts = counts.map { case (tag, count) => (count, tag)}.transform(rdd => rdd.sortByKey(false))
+    sortedCounts.foreachRDD(rdd => println("\nTop 10 hashtags:\n" + rdd.take(10).mkString("\n")))
+
+    ssc.start()
+
+//    Future {
+//      Thread.sleep(1000 * 60 * 10);
+//      ssc.stop(true, true)
+//    }
+
+    ssc.awaitTermination(1000)
   }
 }
-
-//class TwitterFeedReceiver extends Receiver
